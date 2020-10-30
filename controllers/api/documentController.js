90da85feb4cbf7da897b4d75a9b5b43fe249dadd
Nikita Luksha <nikita.luksha@softensity.com>
2020-10-30 14:33:59 +0300

    Get updates

--- controllers/api/documentController.js
+++ controllers/api/documentController.js
@@ -4,28 +4,38 @@ const MongoClient = require('mongodb').MongoClient;
 const config = require('../../dal/config');
 const documentUrl = 'https://apistaging.collaborate.center/swagger/v1/swagger.json';
 
+
+
 exports.getDocument = async function (req, res) {
     let dbClient = null;
   
     try {
       const response = await axios.get(documentUrl);
+      const newApiDocument = response.data;
 
       const client = new MongoClient(config.uri, { useNewUrlParser: true });
       await client.connect();
   
       dbClient = client;
       const documents = dbClient.db(config.dbName).collection('Documents');
-  
-      const resultDoc = await documents.insertOne({
-        url: documentUrl,
-        document: response.data,
-        version: response.data.info.version,
-        date: new Date(),
-      });
+
+      const [latestDocument] = await documents.find().sort({date:-1}).limit(1).toArray();
+      const oldApiDocument = latestDocument.document;
+
+      const updates = findDifference(newApiDocument, oldApiDocument);
+
+      if (updates) {
+        const resultDoc = await documents.insertOne({
+          url: documentUrl,
+          document: newApiDocument,
+          version: newApiDocument.info.version,
+          date: new Date(),
+        });
+      }
 
       res.json({
         success: true,
-        updates: [],
+        updates
       });
     } catch (error) {
       res.status(500).json({
@@ -36,3 +46,21 @@ exports.getDocument = async function (req, res) {
       dbClient && dbClient.close();
     }
 };
+
+
+function findDifference(newDoc, oldDoc) {
+  const newApiList = Object.getOwnPropertyNames(newDoc.paths);
+  const oldApiList = Object.getOwnPropertyNames(oldDoc.paths);
+
+  const added = newApiList.filter(x => !oldApiList.includes(x));
+  const removed = oldApiList.filter(x => !newApiList.includes(x));
+
+  if (added.length === 0 && removed.length === 0) {
+    return null;
+  }
+
+  return {
+    added,
+    removed
+  }
+}
