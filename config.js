5e839662da59e93f885e0f0bf48d4582bd8ee4c5
Mikita Luksha <nikita.luksha@softensity.com>
2024-01-06 15:20:16 +0300

    Update config: DB_User, DB_Password

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.host = 'https://nkt-core-datamanager.onrender.com';
 
-exports.mongoDBURL = `mongodb+srv://${process.env.USER}:${encodeURIComponent(process.env.PASSWORD)}@cluster0.rnxp1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+exports.mongoDBURL = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASSWORD)}@cluster0.rnxp1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 exports.mongoDbName = process.env.DB_NAME;
 
 exports.slackChanelId = process.env.SLACK_CHANEL_ID;
