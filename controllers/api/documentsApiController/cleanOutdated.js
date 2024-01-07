a0f77b9e39f7032be78c58aa365e6bcb390d85d3
Mikita Luksha <nikita.luksha@softensity.com>
2024-01-07 11:50:29 +0300

    Cleanup documents

--- controllers/api/documentsApiController/cleanOutdated.js
+++ controllers/api/documentsApiController/cleanOutdated.js
@@ -10,20 +10,22 @@ exports.cleanOutdated = async function (req, res) {
 
     dbClient = client;
     const documents = dbClient.db(config.mongoDbName).collection('Documents');
-    documentsCount = await documents.countDocuments();
 
-    if (documentsCount > config.messagesToKeepNumber) {
-      const latestDocuments = await documents.find().sort({date:-1}).limit(config.messagesToKeepNumber).toArray();
+    const documentUrls = await getDocumentUrls(documents);
 
-      const lastFromLatestDocument = latestDocuments[latestDocuments.length - 1];
+    for (let i = 0; i < documentUrls.length; i++) {
+      const url = documentUrls[i];
+      const count = await getDocumentsCountByUrl(documents, url);
 
-      await documents.deleteMany({date: {$lt:lastFromLatestDocument.date}});
+      if (count > config.messagesToKeepNumber) {
+        const lastValidDate = await getLastValidDateByUrl(documents, url);
+        await removeExpiredDocuments(documents, url, lastValidDate);
+      }
     }
 
     res.json({
       success: true,
     });
-
   } catch (error) {
     console.error(error);
     res.status(400).json({
@@ -34,3 +36,31 @@ exports.cleanOutdated = async function (req, res) {
     dbClient && await dbClient.close();
   }
 };
+
+async function getDocumentUrls(documents) {
+  const urls = await documents.distinct("url");
+
+  return urls;
+}
+
+async function getDocumentsCountByUrl(documents, url) {
+  const count = await documents.countDocuments({ url });
+
+  return count;
+}
+async function getLastValidDateByUrl(documents, url) {
+  const latestDocuments = await documents
+    .find({ url }, { projection: { date: 1 }})
+    .sort({ date:-1 })
+    .limit(config.messagesToKeepNumber)
+    .toArray();
+
+  const lastFromLatestDocument = latestDocuments[latestDocuments.length - 1];
+  const lastValidDate = lastFromLatestDocument.date;
+
+  return lastValidDate;
+}
+
+async function removeExpiredDocuments(documents, url, lastValidDate) {
+  await documents.deleteMany({ url, date: { $lte:lastValidDate }});
+}
