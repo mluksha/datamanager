d8268c59390634d5e066783bee16c75ffa29a6f3
Mikita Luksha <nikita.luksha@softensity.com>
2024-01-07 10:54:13 +0300

    Add typescript page

--- controllers/documentsController.js
+++ controllers/documentsController.js
@@ -34,12 +34,33 @@ exports.getDocument = async function(req, res) {
     dbClient = client;
 
     const documents = dbClient.db(config.mongoDbNameE).collection('Documents');
-    document = await documents.findOne(ObjectId(id), { projection: { document:0 }});
+    document = await documents.findOne(ObjectId(id), { projection: { document: 0 }});
   } catch (error) {
     console.error(error);
   } finally {
     dbClient && await dbClient.close();
   }
 
-  res.render('document', { title: id, document});
+  res.render('document', { title: id, documentId: id, document});
+};
+
+exports.documentTypeScript = async function(req, res) {
+  const id = req.params.id;
+  let dbClient = null;
+  let document = null;
+  
+  try {
+    const client = new MongoClient(config.mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    dbClient = client;
+
+    const documents = dbClient.db(config.mongoDbNameE).collection('Documents');
+    document = await documents.findOne(ObjectId(id), { projection: { diff: 0 }});
+  } catch (error) {
+    console.error(error);
+  } finally {
+    dbClient && await dbClient.close();
+  }
+
+  res.render('documentTypeScript', { title: id, documentId: id, document});
 };
--- routes/documents.js
+++ routes/documents.js
@@ -5,5 +5,6 @@ const documentsController = require('../controllers/documentsController');
 
 router.get('/', documentsController.getAll);
 router.get('/:id', documentsController.getDocument);
+router.get('/:id/typescript', documentsController.documentTypeScript);
 
 module.exports = router;
